c2324c6b1d14a541fb1516d725c807095ed17527
Franco Pipieri <pipierif98@gmail.com>
2023-03-20 20:03:22 -0300

    agregando env

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ const config = {
     client: "mysql",
     connection: {
         host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || "3000",
         user: process.env.DB_USER || "root",
         password: process.env.DB_PASSWORD || "",
         database: process.env.DB_NAME || "mibase",
